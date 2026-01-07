9f764d940ca9f936d32fd4a78c40dce364d3ad8e
Elysia-alc <1433934034@qq.com>
2026-01-08 01:06:53 +0800

    提交

--- python_learn/datatype.py
+++ python_learn/datatype.py
@@ -8,3 +8,14 @@ string（字符串）、int（整数）、float（浮点数）
 print(type(1))
 print(type(13.14))
 print(type("hello"))
+
+int_type = type(123)
+float_type = type(13.14)
+string_type = type("hello")
+print(int_type)
+print(float_type)
+print(string_type)
+
+name = "Alice"
+name_type = type(name)
+print(name_type) 

7eb6960eab4543545c5583005dba98aa1b8be388
Elysia-alc <1433934034@qq.com>
2026-01-08 20:23:29 +0800

    提交

---
+++ python_learn/string_concatenation.py
@@ -0,0 +1,4 @@
+# 字符串拼接
+str1 = "Hello"
+print(str1 + " World")
+# 无法和非字符串类型进行拼接
---
+++ python_learn/string_definition.py
@@ -0,0 +1,17 @@
+# 字符串定义
+# 单引号定义字符串
+# str1 = 'Hello World'
+
+# 双引号定义字符串
+str2 = "Hello World"
+print(str2)
+
+# 三引号定义字符串（可以定义多行字符串）
+str3 = """
+Hello 
+World
+"""
+print(str3)
+
+# 转义字符定义字符串
+# str4 = "\"Hello World\""
